5be3c8b1cc644441d2e7114ead998863418c333e
khalo <khalo@Khalo.lan>
2025-02-04 08:28:29 +0200

    Initial Commit

---
+++ TaskNotificationApp/src/test/java/com/khalomokwena/TaskNotificationApp/TaskNotificationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khalomokwena.TaskNotificationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskNotificationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
